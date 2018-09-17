c1873d332549d7cb0dddcd8f4f2a96aa8dc8b20f
pulkitkalra <pul.k3333@gmail.com>
2018-09-17 14:00:20 +1200

    Added required constraints to media field to enable cascase deletion

---
+++ InspectionReport/Migrations/20180917015906_MediaEntityConstraints.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InspectionReport.Migrations
+{
+    public partial class MediaEntityConstraints : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Media_Feature_FeatureId",
+                table: "Media");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MediaName",
+                table: "Media",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "FeatureId",
+                table: "Media",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Media_Feature_FeatureId",
+                table: "Media",
+                column: "FeatureId",
+                principalTable: "Feature",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Media_Feature_FeatureId",
+                table: "Media");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MediaName",
+                table: "Media",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<long>(
+                name: "FeatureId",
+                table: "Media",
+                nullable: true,
+                oldClrType: typeof(long));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Media_Feature_FeatureId",
+                table: "Media",
+                column: "FeatureId",
+                principalTable: "Feature",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
--- InspectionReport/Models/Media.cs
+++ InspectionReport/Models/Media.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,9 @@ namespace InspectionReport.Models
     public class Media
     {
         public long Id { get; set; }
+        [Required]
         public Feature Feature { get; set; }
+        [Required]
         public string MediaName { get; set; } 
     }
 }
