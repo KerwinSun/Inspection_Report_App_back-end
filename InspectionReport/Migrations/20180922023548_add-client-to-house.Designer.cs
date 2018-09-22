9c67d876a4c100ca85e96bd9b3ba7edc7c647a8a
Karim-C <kcis880@aucklanduni.ac.nz>
2018-09-22 16:57:03 +1200

    add migrations

--- InspectionReport/Migrations/20180922023548_add-client-to-house.Designer.cs
+++ InspectionReport/Migrations/20180922023548_add-client-to-house.Designer.cs
@@ -4,14 +4,16 @@ using InspectionReport.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace InspectionReport.Migrations
 {
     [DbContext(typeof(ReportContext))]
-    partial class TodoContextModelSnapshot : ModelSnapshot
+    [Migration("20180922023548_add-client-to-house")]
+    partial class addclienttohouse
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
